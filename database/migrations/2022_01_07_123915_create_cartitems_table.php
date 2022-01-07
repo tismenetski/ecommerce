b2183c35373a9fea243f07fb522f8bfd99adc78f
tismenetski <tismenetski@gmail.com>
2022-01-07 14:45:47 +0200

    added cart migrations

---
+++ database/migrations/2022_01_07_123915_create_cartitems_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartitemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cartitems', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->bigInteger('cart_id')->unsigned()->nullable();
+            $table->foreign('cart_id')->references('id')->on('carts')->onDelete('cascade');
+            $table->bigInteger('product_id')->unsigned()->nullable();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cartitems');
+    }
+}
