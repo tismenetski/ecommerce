7c390cc94135a5a1c0f239196e00b488e4307fb3
tismenetski <tismenetski@gmail.com>
2022-01-07 14:27:22 +0200

    Create Auth Controller

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'city',
+        'state',
+        'country',
+        'address',
+        'address2',
+        'phone'
     ];
 
     /**
